a648c1eb6bb9e14e9b0a5f7155a8fa0d2515c062
atishay-nar <atishayna@gmail.com>
2025-06-21 23:46:10 -0700

    fleshed out mha. still need proper single head attention

--- qmsan/mathematical.py
+++ qmsan/mathematical.py
@@ -35,20 +35,27 @@ def single_qmsan(Q, K, V, dev):
                 score = fidelity(rho_q, rho_k)
                 attn[b][i][j] = score
     
-    # scale attnetion scores to range [-1, 1]
+    # scale attention scores to range [-1, 1]
     attn = 2 * attn - np.ones_like(attn)
-    # to do: mask to avoid peaking at future tokens
+    # to do: mask to avoid peaking at future tokens. before or after scaling?
     return attn @ V
 
 # torch module of single-head quantum mixed-state self-attention
 class SingleQMSANHead(nn.Module):
-    def __init__(self, dev):
+    def __init__(self, embed_dim, head_dim, dev):
         super().__init__()
+        
+        self.head_dim =  head_dim
 
         # embeds Q, K, V into quantum states and returns their mixed states
         self.circuit = qml.QNode(get_mixed_state, dev, interface="torch") 
 
+        # classical linear projection for V since we matmul post-measurement
+        self.V_linear = nn.Linear(embed_dim, self.head_dim, bias=False)
+
     def forward(self, Q, K, V): # TO DO: change this, this sucks. figure out how to embed Q, K, V for an x vector
+
+        # V = self.V_linear(x)
         
         # batch size and number of tokens per attention head
         B, S, E = Q.shape
@@ -69,13 +76,34 @@ class SingleQMSANHead(nn.Module):
                     score = fidelity(rho_q, rho_k)
                     attn[b][i][j] = score
         
-        # scale attnetion scores to range [-1, 1]
+        # scale attention scores to range [-1, 1]
         attn = 2 * attn - torch.ones_like(attn, dtype=torch.float32)
 
-        # TO DO: mask to avoid atteneding on future tokens
-        return torch.matmul(attn, V)
+        out = torch.matmul(attn, V)
 
+        # TO DO: mask to avoid attending on future tokens
+        return out
     
+# multi-headed QMSAN
+class MultiHeadQMSAN(nn.Module):
+    def __init__(self, embed_dim, n_heads, dev):
+        super().__init__()
+        assert embed_dim % n_heads == 0, "Embed dimension must be divisible by number of heads"
+        self.head_size = embed_dim // n_heads
+
+        # final linear projection for MHA
+        self.W_o = nn.Linear(embed_dim, embed_dim)
+
+        # single-head QMSAN heads
+        self.heads = nn.ModuleList([SingleQMSANHead(embed_dim, self.head_size, dev) for _ in range(n_heads)])
+
+    def forward(self, x):
+        # concatenate all heads
+        out = torch.cat([head(x)for head in self.heads], dim=-1)
+
+        # linear proj
+        out = self.W_o(out)
+        return out
 
 if __name__ == "__main__":
     dict = yaml.safe_load(open("configs.yml", "r"))
@@ -89,7 +117,7 @@ if __name__ == "__main__":
     print(single_qmsan(Q, K, V, dev))
 
     # test torch module
-    module = SingleQMSANHead(dev)
+    module = SingleQMSANHead(cfg.EMBED_DIM, cfg.NUM_HEADS, dev)
     print(module(
                 torch.tensor(Q, dtype=torch.float32), 
                 torch.tensor(K, dtype=torch.float32), 
