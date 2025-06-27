e5bb14334a848076e6e82253d7398754d96479e4
atishay-nar <atishayna@gmail.com>
2025-06-27 14:25:57 -0700

    finished sample function and got initial samples

--- src/sample.py
+++ src/sample.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-import torchvision
+import pennylane as qml
 from torchvision import transforms
 from PIL import Image
 from reduced_mnist import ReducedMNISTDataset
@@ -27,7 +27,7 @@ def generate(model, context, length, num_samples=1):
 
     return output
 
-def sample(model, checkpoint_num, device, centroid_dir, num_clusters, train_data, img_size, n_examples=5, n_samples=5):
+def sample(model, checkpoint_num, device, centroid_dir, num_clusters, test_data, img_size, n_examples=3, n_samples=3):
     os.makedirs('./figures', exist_ok=True)
 
     # load model checkpoint
@@ -39,13 +39,13 @@ def sample(model, checkpoint_num, device, centroid_dir, num_clusters, train_data
     centroids_path = os.path.join(centroid_dir, f"centroids_{num_clusters}.npy")
     centroids = torch.tensor(np.load(centroids_path)).to(device)
 
-    loader = iter(DataLoader(train_data, batch_size=1, shuffle=True))
+    loader = iter(DataLoader(test_data, batch_size=1, shuffle=True))
 
 
     rows=[]
     for example in tqdm(range(n_examples), desc="Sampling Images"):
         # get random image
-        img, _ = next(loader)
+        img = next(loader)
         img = img[0].to(device)
 
         # quantize image to tokens
@@ -80,3 +80,40 @@ if __name__ == "__main__":
     parser.add_argument("--n_samples", default=5, type=int)
     args, _ = parser.parse_known_args()
     cfg = argparse.Namespace(**yaml.safe_load(open("configs.yml", "r")))
+
+    # set device
+    DEVICE = (
+        "cuda" if torch.cuda.is_available()
+        # else "mps" if torch.mps.is_available()
+        else "cpu"
+        )
+    
+    # set quantum device
+    num_qubits = int(cfg.EMBED_DIM / cfg.NUM_HEADS)
+
+    if torch.cuda.is_available():
+        dev = qml.device("lightning.gpu", wires=num_qubits)
+    else:
+        dev = qml.device("default.mixed", wires=num_qubits)
+
+    # define model
+    model = QuantumImageGPT(
+        vocab_size=cfg.NUM_CLUSTERS,
+        embed_dim=cfg.EMBED_DIM,
+        n_heads=cfg.NUM_HEADS,
+        n_layers=cfg.NUM_LAYERS,
+        image_size=cfg.IMAGE_SIZE,
+        quantum_device=dev
+    ).to(DEVICE)
+
+    test_data = ReducedMNISTDataset(cfg.IMAGE_SIZE, classes=cfg.CLASSES, samples_per_class=cfg.SAMPLES_PER_CLASS, train=False)
+
+    sample(
+        model=model,
+        checkpoint_num=args.ckpt_num,
+        device=DEVICE,
+        centroid_dir=cfg.CENTROID_DIR,
+        num_clusters=cfg.NUM_CLUSTERS,
+        test_data=test_data,
+        img_size=cfg.IMAGE_SIZE,
+    )
